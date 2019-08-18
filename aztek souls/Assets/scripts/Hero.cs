bba47a09b903edcb5f625f8bbe95ac63ce58a498
IgnacioIngold <43892862+IgnacioIngold@users.noreply.github.com>
2019-08-18 17:20:25 -0300

    movimiento(mejorar)
    bases de movimiento.

--- aztek souls/Assets/scripts/Hero.cs
+++ aztek souls/Assets/scripts/Hero.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float Speed;
+    Animator _am;
+
+    void Awake()
     {
-        
+        _am = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    public void Move()
+    {
+        float dirX = Input.GetAxis("Horizontal");
+        float dirY = Input.GetAxis("Vertical");
+        var dir = new Vector3(dirX,0,dirY);
+
+        transform.position += dir * Speed;
+
+        _am.SetFloat("VelY", dirY);
+        _am.SetFloat("VelX", dirX);
     }
 }
