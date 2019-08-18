89ca0af6370f7846ebf9a2ce04d806186bb24131
IgnacioIngold <43892862+IgnacioIngold@users.noreply.github.com>
2019-08-18 18:17:05 -0300

    retoque movimientos

--- aztek souls/Assets/scripts/Hero.cs
+++ aztek souls/Assets/scripts/Hero.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    public float Speed;
+    public float speed;
     Animator _am;
+    float _dirX;
+    float _dirY;
+    Vector3 _dir;
+
 
     void Awake()
     {
@@ -21,13 +25,23 @@ public class Hero : MonoBehaviour
 
     public void Move()
     {
-        float dirX = Input.GetAxis("Horizontal");
-        float dirY = Input.GetAxis("Vertical");
-        var dir = new Vector3(dirX,0,dirY);
+        _dirX = Input.GetAxis("Horizontal");
+        _dirY = Input.GetAxis("Vertical");
+        _dir = Vector3.Normalize(new Vector3(_dirX, 0, _dirY));
 
-        transform.position += dir * Speed;
+        Debug.Log("Vertical" + Input.GetAxis("Vertical") + "Horizontal" + Input.GetAxis("Horizontal"));
 
-        _am.SetFloat("VelY", dirY);
-        _am.SetFloat("VelX", dirX);
+        if (Mathf.Abs(Input.GetAxis("Horizontal"))
+             >0.5 && Mathf.Abs(Input.GetAxis("Vertical")) >0.5)
+        {
+            transform.position += _dir * speed / 2;
+            
+        }
+            
+
+        transform.position += _dir * speed;
+
+        _am.SetFloat("VelY", _dirY,0.2f,Time.deltaTime*2);
+        _am.SetFloat("VelX", _dirX, 0.2f, Time.deltaTime * 2);
     }
 }
