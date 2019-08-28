2aee3a100352f582b1149e0fd80c2c5b70c9cfa5
Kirtap98 <49925184+Kirtap98@users.noreply.github.com>
2019-08-28 16:38:24 -0300

    Blood Particle
     aun no esta en ningun enemigo falta activacion nomas ya esta la particula

---
+++ aztek souls/Assets/Shaders/ScriptTest/parti.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parti : MonoBehaviour
+{
+    public ParticleSystem particleLauncher;
+    private void Awake()
+    {
+        
+    }
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            particleLauncher.Emit(1);
+
+        }
+        
+    }
+
+    
+}
