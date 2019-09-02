623880b6c9a56301ab9fbfa5e70e1c0b64f9ab8a
IgnacioIngold <43892862+IgnacioIngold@users.noreply.github.com>
2019-09-02 19:45:41 -0300

    pottbreack

---
+++ aztek souls/Assets/scripts/Breackable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breackable : MonoBehaviour
+{
+    public GameObject BreackObject;
+
+    private void Awake()
+    {
+        StartCoroutine(Breakpott());
+    }
+    public void Break()
+    {
+        Instantiate(BreackObject, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    IEnumerator Breakpott()
+    {
+        Debug.Log("entre");
+        yield return new WaitForSeconds(1f);
+        Break();
+
+    }
+   
+}
