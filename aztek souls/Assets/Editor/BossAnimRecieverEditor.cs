f51431f4b25f47bbcc9ce42ccff2db7c7a5b5f3d
DenisVargas <denis.vargasrivero@outlook.com>
2019-09-11 23:08:52 -0300

    Transform Animation Receiver

--- aztek souls/Assets/Editor/BossAnimRecieverEditor.cs
+++ aztek souls/Assets/Editor/BossAnimRecieverEditor.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEditor;
 
@@ -8,14 +8,18 @@ public class BossAnimRecieverEditor : Editor
 {
     BossAnimEvents inspected;
     bool foldout;
+    int selectedIndex = 0;
 
-    Color selectedColor;
-    AnimationCurve bla;
-    private int selectedLayer;
-    private Gradient selectedGradient = new Gradient();
-    private string selectedTag;
-    private string historyText;
-    private Bounds selectedBounds;
+    //Warnings
+    bool AEReciever = true;
+    bool colliderSetted = true;
+    bool FolderExists = true;
+    bool FileExists = true;
+
+    //Debug.
+    bool FoldOut_DebugMessages = true;
+    bool DebugMessages = true;
+    bool CopiedSettings = false;
 
     SerializedProperty ColliderValues;
 
@@ -23,80 +27,171 @@ public class BossAnimRecieverEditor : Editor
     {
         inspected = (BossAnimEvents)target;
 
-        ColliderValues = serializedObject.FindProperty("DamageCollider");
-        //Cargo la posible data.
+        //Chequear que el collider este seteado.
+        AEReciever = inspected.boss != null;
+        colliderSetted = inspected.DamageCollider != null;
+
+        CheckRelevantData();
+
+        if (FolderExists && FileExists)
+            inspected.loadData(BossAnimEvents.CompleteDataPath);
+
+        //cambiar el valor de selected index segun la cantidad de elementos cargados.
+        if (inspected.ColliderTransformKeyFrame == null)
+            inspected.ColliderTransformKeyFrame = new Dictionary<int, BossAnimEvents.TransformValues>();
+        selectedIndex = inspected.ColliderTransformKeyFrame.Count;
+
+    }
+
+    private void CheckRelevantData()
+    {
+        selectedIndex = inspected.ColliderTransformKeyFrame.Count;
+
+        //Chequear que el collider y el Observador esten seteados.
+        AEReciever = inspected.boss != null;
+        colliderSetted = inspected.DamageCollider != null;
+
+        //Chequear que la carpeta exista y el archivo existe.
+        FolderExists = Directory.Exists(Application.dataPath + BossAnimEvents.DataPath);
+        FileExists = File.Exists(Application.dataPath + BossAnimEvents.CompleteDataPath);
     }
 
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
 
-        //Dibujo antes del base.
+        CheckRelevantData();
+
+        #region Mensajes de advertencia
+        //Mostramos los mensajes de advertencia en caso de que la data mas relevante tenga algún problema.
+        if (!colliderSetted || !AEReciever)
+        {
+            string Advertencies = "Algunos elementos no han sido seteados todavía!:";
+            if (!AEReciever) Advertencies += "\nEl Observador no ha sido seteado.";
+            if (!colliderSetted) Advertencies += "\nEl collider no ha sido seteado.";
+            Advertencies += "\nEsto es obligatorio.";
+
+            EditorGUILayout.HelpBox(Advertencies, MessageType.Error);
+        }
+
+        if (!FolderExists || !FileExists)
+        {
+            string Advertencies = "There is a problem with the Data!";
+            if (!FolderExists) Advertencies += "\nData Path folder does not exists";
+            if (!FileExists) Advertencies += "\nData File does not exists";
+
+            EditorGUILayout.HelpBox(Advertencies, MessageType.Warning);
+            if (GUILayout.Button("Create new DataFile"))
+            {
+                Directory.CreateDirectory(Application.dataPath + BossAnimEvents.DataPath);
+                inspected.SaveData();
+                AssetDatabase.Refresh();
+
+                if (DebugMessages)
+                    MonoBehaviour.print("Archivo y Path creados");
+            }
+        } 
+        #endregion
+
+        EditorGUI.BeginDisabledGroup(!colliderSetted || !AEReciever);
         GUILayout.Space(10f);
         GUILayout.BeginHorizontal();                          //Inicio grupo Horizontal.
 
             GUI.color = Color.green;
-            if (GUILayout.Button("Copy Target Collider Settings"))
+            if (GUILayout.Button("Copy Target Collider Settings") && colliderSetted)
             {
-                //Expando con esto :D
+            #region Debug Copied Settings
+            if (CopiedSettings)
+            {
+                string Settings = "Damage Collider Current Settings:\n" +
+                                         "Position = ({0};{1};{2})\n" +
+                                         "Rotation = ({3};{4};{5})\n" +
+                                         "Scale = ({6};{7};{8})";
+
+                Transform col = inspected.DamageCollider.transform;
+                Settings = string.Format(Settings,
+                                         col.localPosition.x, col.localPosition.y, col.localPosition.z,
+                                         col.rotation.x, col.rotation.y, col.rotation.z,
+                                         col.localScale.x, col.localScale.y, col.localScale.z);
+
+                Debug.LogFormat(Settings);
+            } 
+            #endregion
+
+                //Guardo los valores en un contenedor.
+                BossAnimEvents.TransformValues serializedValues = new BossAnimEvents.TransformValues()
+                {
+                    localPosition = inspected.DamageCollider.transform.localPosition,
+                    rotation = inspected.DamageCollider.transform.rotation,
+                    localScale = inspected.DamageCollider.transform.localScale
+                };
+
+                //Los añado al diccionario.
+                if (inspected.ColliderTransformKeyFrame.ContainsKey(selectedIndex))
+                {
+                    inspected.ColliderTransformKeyFrame[selectedIndex] = serializedValues;
+                    if (DebugMessages)
+                        MonoBehaviour.print("Los valores han sido reemplazados.");
+                }
+                else
+                {
+                    inspected.ColliderTransformKeyFrame.Add(selectedIndex, serializedValues);
+                    if (DebugMessages)
+                        MonoBehaviour.print("Los valores han sido añadidos.");
+                }
             }
             GUI.color = Color.grey;
 
-            EditorGUILayout.FloatField(0f);
+            selectedIndex = EditorGUILayout.IntField(selectedIndex);
 
         GUILayout.EndHorizontal();                              //Fin Grupo Horizontal.
         GUILayout.Space(10f);
 
-        /*
-         * GUILayout.Button("TestButton");
-            GUILayout.Button("TestButton");
-            GUILayout.Button("TestButton");
-         */
-
-        foldout = EditorGUILayout.BeginFoldoutHeaderGroup(foldout, "Info");
-        if (foldout)
+        if (GUILayout.Button("Save Data"))
         {
-            selectedColor = EditorGUILayout.ColorField("Pick a color", Color.white);
-            bla = EditorGUILayout.CurveField("Pick a Curve (?)", bla);
-            selectedLayer = EditorGUILayout.LayerField(selectedLayer);
-            selectedGradient = EditorGUILayout.GradientField(selectedGradient);
-            selectedTag = EditorGUILayout.TagField(selectedTag);
-            historyText = EditorGUILayout.TextArea(historyText);
-            selectedBounds = EditorGUILayout.BoundsField(selectedBounds);
-            EditorGUILayout.Space();
-            EditorGUILayout.PropertyField(ColliderValues);
+            if (DebugMessages) MonoBehaviour.print("Data has been saved...");
+            inspected.SaveData();
         }
-        EditorGUILayout.EndFoldoutHeaderGroup();
+
+        EditorGUI.EndDisabledGroup();
+
+        #region Debug
+
+        //Enable And Debug Messages
+        FoldOut_DebugMessages = EditorGUILayout.Foldout(FoldOut_DebugMessages, "Debug Messages");
+        if (FoldOut_DebugMessages)
+        {
+            DebugMessages = EditorGUILayout.ToggleLeft("Print Debug Messages", DebugMessages);
+            CopiedSettings = EditorGUILayout.ToggleLeft("Print Copied Values.", CopiedSettings);
+        }
+
+        #endregion
 
         GUI.color = Color.white;
     }
 
-    [ContextMenu("Add Current Collider Settings")]
-    void CopyColliderSettings()
-    {
-        if (inspected.DamageCollider == null)
-        {
-            print("El collider no esta Seteado Salamin");
-            return;
-        }
+    //=========================================================================================
 
-        string Settings = "Damage Collider Current Settings:\n" +
-                                 "Position = ({0};{1};{2})\n" +
-                                 "Rotation = ({3};{4};{5})\n" +
-                                 "Scale = ({6};{7};{8})";
+    //[ContextMenu("Add New Transform Collider Setting")]
+    //void CopyColliderSettings()
+    //{
+    //    if (inspected.DamageCollider == null)
+    //    {
+    //        Debug.LogError("El collider no esta Seteado Salamin");
+    //        return;
+    //    }
 
-        Transform col = inspected.DamageCollider.transform;
-        Settings = string.Format(Settings,
-                                 col.localPosition.x, col.localPosition.y, col.localPosition.z,
-                                 col.rotation.x, col.rotation.y, col.rotation.z,
-                                 col.localScale.x, col.localScale.y, col.localScale.z);
+    //    string Settings = "Damage Collider Current Settings:\n" +
+    //                             "Position = ({0};{1};{2})\n" +
+    //                             "Rotation = ({3};{4};{5})\n" +
+    //                             "Scale = ({6};{7};{8})";
 
-        print(Settings);
-    }
+    //    Transform col = inspected.DamageCollider.transform;
+    //    Settings = string.Format(Settings,
+    //                             col.localPosition.x, col.localPosition.y, col.localPosition.z,
+    //                             col.rotation.x, col.rotation.y, col.rotation.z,
+    //                             col.localScale.x, col.localScale.y, col.localScale.z);
 
-    void print(object message)
-    {
-        MonoBehaviour.print(message);
-    }
-
+    //    Debug.LogWarning(Settings);
+    //}
 }
--- aztek souls/Assets/scripts/Units/Enemies/Boss/BossAnimEvents.cs
+++ aztek souls/Assets/scripts/Units/Enemies/Boss/BossAnimEvents.cs
@@ -1,12 +1,62 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using Core.Serialization;
 
 public class BossAnimEvents : MonoBehaviour
 {
+    public const string DataPath = "/Resources/Data";
+    public const string FileAndExtention = "BossColliderAnims.json";
+    public const string CompleteDataPath = "/Resources/Data/BossColliderAnims.json";
+
+    public Dictionary<int, TransformValues> ColliderTransformKeyFrame = new Dictionary<int, TransformValues>();
+
     public BigCursed boss;
     public Collider DamageCollider;
+
+    [Serializable]
+    public struct DataPairContainter
+    {
+        public List<int> Keys;
+        public List<TransformValues> values;
+    }
+
+    [Serializable]
+    public struct TransformValues
+    {
+        public Vector3 localPosition;
+        public Quaternion rotation;
+        public Vector3 localScale;
+    }
+
+    public void loadData(string dataPath)
+    {
+        if (ColliderTransformKeyFrame == null)
+            ColliderTransformKeyFrame = new Dictionary<int, TransformValues>();
+
+        ColliderTransformKeyFrame.Clear();
+
+        string completePath = Application.dataPath + CompleteDataPath;
+        DataPairContainter data = FullSerialization.Deserialize<DataPairContainter>(completePath, false);
+
+        for (int i = 0; i < data.Keys.Count; i++)
+            ColliderTransformKeyFrame.Add(data.Keys[i], data.values[i]);
+    }
+    public void SaveData()
+    {
+        string completePath = Application.dataPath + CompleteDataPath;
+        DataPairContainter data = new DataPairContainter() { Keys = new List<int>(), values = new List<TransformValues>()};
+
+        foreach (var pair in ColliderTransformKeyFrame)
+        {
+            data.Keys.Add(pair.Key);
+            data.values.Add(pair.Value);
+        }
+
+        data.Serialize(completePath, false);
+    }
 
     void StartBasicCombo()
     {
@@ -59,6 +109,4 @@ public class BossAnimEvents : MonoBehaviour
     {
 
     }
-
-    
 }
