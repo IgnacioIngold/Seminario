31c557b66688de98b6b3e520c9cd36f249ea28e2
IgnacioIngold <43892862+IgnacioIngold@users.noreply.github.com>
2019-09-13 05:12:02 -0300

    timeline

---
+++ aztek souls/Assets/scripts/Utility/ActivateTimeline.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ActivateTimeline : MonoBehaviour
+{
+    public PlayableDirector mytimeline;
+    bool HasBeenActive;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Player>() != null && !HasBeenActive)
+        {
+            mytimeline.Play();
+            HasBeenActive = true;
+        }
+    }
+}
