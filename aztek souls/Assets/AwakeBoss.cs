2b2dfb2f36a8a148e8d4f10971a6540285312ac4
DenisVargas <denis.vargasrivero@outlook.com>
2019-09-13 01:31:26 -0300

    AwakeBoss
    Añadido un trigger que permite habilitar a los 2 Bosses solo cuando cruzamos las escaleras!

---
+++ aztek souls/Assets/AwakeBoss.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AwakeBoss : MonoBehaviour
+{
+    Player p;
+    public UnityEvent OnPlayerIsInPlace;
+
+    private void Awake()
+    {
+        p = GameObject.FindObjectOfType<Player>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Player>() != null)
+            OnPlayerIsInPlace.Invoke();
+    }
+}
