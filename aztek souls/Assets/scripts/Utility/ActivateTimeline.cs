4e6639fbca4d0ffc28ef7e5c85b1b047712c30d7
DenisVargas <denis.vargasrivero@outlook.com>
2019-09-15 12:28:10 -0300

    Fix Pequeño detashe jejej

--- aztek souls/Assets/scripts/Utility/ActivateTimeline.cs
+++ aztek souls/Assets/scripts/Utility/ActivateTimeline.cs
@@ -7,13 +7,13 @@ public class ActivateTimeline : MonoBehaviour
 {
     public PlayableDirector mytimeline;
     bool HasBeenActive;
-    public GameObject block;
+    //public GameObject block;
     private void OnTriggerEnter(Collider other)
     {
         if(other.GetComponent<Player>() != null && !HasBeenActive)
         {
             mytimeline.Play();
-            block.SetActive(true);
+            //block.SetActive(true);
             HasBeenActive = true;
         }
     }
