29b8dc7b7a4fb019f07e40ed9120297cb75b9163
Kirtap98 <49925184+Kirtap98@users.noreply.github.com>
2019-10-03 22:19:40 -0300

    Replacement shader apply

---
+++ aztek souls/Assets/Shaders/replacement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class replacement : MonoBehaviour
+{
+    public Shader XRayShader;
+    private void OnEnable()
+    {
+        GetComponent<Camera>().SetReplacementShader(XRayShader, "XRay");
+    }
+}
