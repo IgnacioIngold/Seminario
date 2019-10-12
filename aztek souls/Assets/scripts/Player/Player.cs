97d63f494e9cf254b2726a66601e94ce2e324d9a
IgnacioIngold <43892862+IgnacioIngold@users.noreply.github.com>
2019-10-12 18:44:42 -0300

    arreglo roll

--- aztek souls/Assets/scripts/Player/Player.cs
+++ aztek souls/Assets/scripts/Player/Player.cs
@@ -847,8 +847,8 @@ public class Player : MonoBehaviour, IPlayerController, IKilleable, IAttacker<ob
         _clamped = true;
         _rolling = true;
         _recoverStamina = false;
-        _running = false;
-        _anims.SetBool("Running", false);
+        //_running = false;
+        //_anims.SetBool("Running", false);
         _invulnerable = true;
 
         //FeedBack
