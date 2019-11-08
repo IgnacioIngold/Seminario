a073caee311343d29896b2de69930f449e51f4db
DenisVargas <denis.vargasrivero@outlook.com>
2019-11-07 23:25:57 -0300

    Update TriggerWeapon.cs
    Añadidos unas condiciones para Excluir variables y llamadas a funciones innecesarias para la build.

--- aztek souls/Assets/scripts/TriggerWeapon.cs
+++ aztek souls/Assets/scripts/TriggerWeapon.cs
@@ -7,25 +7,27 @@ using Core.Entities;
 //[AddComponentMenu("Core/Trigger Weapon"), RequireComponent(typeof(Collider))]
 public class TriggerWeapon : MonoBehaviour
 {
-    [SerializeField, Tooltip("El collider es desactivado al producirse el primer impacto.")]
-    protected Collider col;
+    //[SerializeField, Tooltip("El collider es desactivado al producirse el primer impacto.")]
+    //protected Collider col;
     public GameObject Owner;
 
+#if UNITY_EDITOR
     public bool debugThisUnit;
-    public bool debugOwnerStats;
+    public bool debugOwnerStats; 
+#endif
 
     IDamageable<HitData, HitResult> _owner;
 
     void Awake()
     {
         var getted = Owner.TryGetComponent(out _owner);
-        if (getted)
-            Debug.LogWarning("Encontrado. " + Owner.gameObject.name);
-        else
-            Debug.LogWarning("No encontrado");
+        //if (getted)
+        //    Debug.LogWarning("Encontrado. " + Owner.gameObject.name);
+        //else
+        //    Debug.LogWarning("No encontrado");
 
-        if (col == null)
-            col = GetComponent<Collider>();
+        //if (col == null)
+        //    col = GetComponent<Collider>();
     }
 
     void OnTriggerEnter(Collider other)
@@ -35,16 +37,20 @@ public class TriggerWeapon : MonoBehaviour
         IDamageable<HitData, HitResult> Target = other.GetComponentInParent<IDamageable<HitData, HitResult>>();
         if (Target != null)
         {
+#if UNITY_EDITOR
             if (debugThisUnit)
-                print(string.Format("Owner: {0}, Colisionó con el siguiente Objeto: {1}", Owner.name, other.gameObject.name));
+                print(string.Format("Owner: {0}, Colisionó con el siguiente Objeto: {1}", Owner.name, other.gameObject.name)); 
+#endif
 
             _owner.FeedHitResult(Target.Hit(_owner.GetDamageStats()));
 
+#if UNITY_EDITOR
             if (debugOwnerStats)
             {
                 var stats = _owner.GetDamageStats();
                 print(string.Format("El daño del owner es {0}, puede romper defenza {1}, tipo de ataque {2}.", stats.Damage, stats.BreakDefence, stats.AttackType.ToString()));
-            }
+            } 
+#endif
 
             return;
         }
