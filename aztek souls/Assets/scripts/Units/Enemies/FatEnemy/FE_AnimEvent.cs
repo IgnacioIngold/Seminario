d726f2ead2585f38a161cbd01f812924fa23db32
DenisVargas <denis.vargasrivero@outlook.com>
2019-11-07 23:27:19 -0300

    Anim Events FatEnemy
    + ImportSettings para las animaciones afectadas.

---
+++ aztek souls/Assets/scripts/Units/Enemies/FatEnemy/FE_AnimEvent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FE_AnimEvent : MonoBehaviour
+{
+    public FatEnemy Owner;
+    public Collider DamageCollider;
+
+    private void Awake()
+    {
+        Owner = GetComponentInParent<FatEnemy>();
+    }
+
+    /// <summary>
+    /// Avisa el momento de Animación en el que el Owner efectúa el Disparo.
+    /// </summary>
+    public void AE_ShootStart()
+    {
+        Owner.Shoot();
+    }
+
+    //============================== Combate ===============================================
+
+    public void StartUp()
+    {
+        //Esto hay que ver que pasa xD.
+    }
+
+    public void Active()
+    {
+        DamageCollider.enabled = true;
+        Owner.LookTowardsPlayer = false;
+    }
+
+    public void Recover()
+    {
+        DamageCollider.enabled = false;
+    }
+}
