1371709196ac52f9150316fec8db79409c4ef192
DenisVargas <denis.vargasrivero@outlook.com>
2019-11-14 23:25:06 -0300

    Multi Step Timer
    Añadido una clase que permite añadir un Timer con multiples pasos, cada paso llama a un Evento OnTimeStart() y al terminar el tiempo llama a un evento OnTimesUp().
    Escala con el tiempo (TimeScale).

---
+++ aztek souls/Assets/scripts/Utility/Debug/TestTimer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Utility.Timers.RunTime;
+
+public class TestTimer : MonoBehaviour
+{
+    [SerializeField] RT_ConditionalMultiStepTimer HitWindows;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HitWindows.OnTimeStart +=() => { print("CountDown Started"); };
+        HitWindows.OnTimesUp += () => { print("TimeIsFinished"); };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HitWindows.isReady)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                HitWindows.Start();
+        }
+
+        if (!HitWindows.isReady)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                HitWindows.NextStep();
+
+            if ((Input.GetKeyDown(KeyCode.S)))
+            {
+                if (HitWindows.isPaused)
+                    HitWindows.Continue();
+                else
+                    HitWindows.Pause();
+            }
+
+            if (Input.GetKeyDown(KeyCode.D))
+                HitWindows.Restart();
+        }
+
+        HitWindows.Update();
+    }
+}
---
+++ aztek souls/Assets/scripts/Utility/RT_ConditionalMultiStepTimer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utility.Timers.RunTime
+{
+    [Serializable]
+    public class RT_ConditionalMultiStepTimer
+    {
+        public Action OnTimeStart = delegate { };
+        public Action OnTimesUp = delegate { };
+
+        [HideInInspector] public bool isReady = true;
+        [HideInInspector] public bool LastStep = false;
+        [HideInInspector] public bool isPaused = false;
+
+        public float[] steps = new float[0];
+        public int Step
+        {
+            get => _currentStep;
+            set
+            {
+                int maxValue = steps.Length > 1 ? steps.Length - 1 : 0;
+                _currentStep = Mathf.Clamp(value, 0, maxValue);
+
+                if (_currentStep == maxValue)
+                    LastStep = true;
+            }
+        }
+        public float normalizedTime
+        {
+            get => (_remainingTime / steps[_currentStep]);
+        }
+
+        int _currentStep;
+        float _remainingTime = 0;
+
+        public void Start()
+        {
+            if (steps.Length == 0 || !isReady) return;
+
+            OnTimeStart();
+            isReady = false;
+            _remainingTime = steps[Step];
+            if (_remainingTime == 0)
+            {
+                Restart();
+                OnTimesUp();
+            }
+        }
+        public void Update()
+        {
+            if (steps.Length == 0) return;
+
+            if (!isReady && !isPaused && _remainingTime > 0)
+            {
+                _remainingTime -= Time.deltaTime;
+
+                if (_remainingTime <= 0)
+                {
+                    OnTimesUp();
+                    Restart();
+                }
+            }
+        }
+
+        public void NextStep()
+        {
+            if (steps.Length == 0) return;
+
+            Step++;
+            if (!LastStep)
+            {
+                _remainingTime = steps[Step];
+                OnTimeStart();
+            }
+        }
+
+        public void Pause()
+        {
+            isPaused = true;
+        }
+        public void Continue()
+        {
+            isPaused = false;
+        }
+        public void Restart()
+        {
+            Step = 0;
+            isReady = true;
+            isPaused = false;
+            LastStep = false;
+
+            if (steps.Length > 0) _remainingTime = steps[0];
+        }
+    }
+}
